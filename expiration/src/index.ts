a2b60f0433d2cb6ba001918e6fe83103b18bbb1e
Tim <borodin.tim@gmail.com>
2021-08-15 20:46:30 +0300

    new startup messages

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,8 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+    console.log('Starting up...');
+    
     if (!process.env.NATS_CLIENT_ID) {
         throw new Error('NATS_CLIENT_ID must be defined');
     }
@@ -12,23 +14,19 @@ const start = async () => {
         throw new Error('NATS_URL must be defined');
     }
 
-    // try {
-        await natsWrapper.connect(
-            process.env.NATS_CLUSTER_ID,
-            process.env.NATS_CLIENT_ID,
-            process.env.NATS_URL
-        );
-        natsWrapper.client.on('close', () => {
-            console.log('NATS connection closed');
-            process.exit();
-        });
-        process.on('SIGINT', () => natsWrapper.client.close());
-        process.on('SIGTERM', () => natsWrapper.client.close());
+    await natsWrapper.connect(
+        process.env.NATS_CLUSTER_ID,
+        process.env.NATS_CLIENT_ID,
+        process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+        console.log('NATS connection closed');
+        process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
 
-        new OrderCreatedListener(natsWrapper.client).listen();
-    // } catch (err) {
-    //     console.error(err);
-    // }
+    new OrderCreatedListener(natsWrapper.client).listen();
 };
 
 start();
