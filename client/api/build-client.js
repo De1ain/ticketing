dced41df47b3100f7ab2d6060479cf8b5e633e14
Tim <borodin.tim@gmail.com>
2021-08-16 18:26:40 +0300

    build-client url changed to prod

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,8 @@ const BuildClient = ({ req }) => {
     if (typeof window === 'undefined') {
         // request comes from the server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.kube-system.svc.cluster.local',
-            // baseURL: 'http://www.ticketing-microservices-dev.xyz/',
+            // baseURL: 'http://ingress-nginx-controller.kube-system.svc.cluster.local',
+            baseURL: 'http://www.ticketing-microservices-dev.xyz/',
             headers: req.headers
         });
     } else {
